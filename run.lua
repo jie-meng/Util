1386aade0ba129010f67ab1d67464fec795fa2a2
Joshua Meng <joshua.meng.xy@gmail.com>
2016-05-29 10:13:57 +0800

    Add run.lua

---
+++ run.lua
@@ -0,0 +1,11 @@
+os.execute("python ./make.py")
+
+print("Running started ...\n")
+
+if strContains(platformInfo(), "win", false) then
+    os.execute("./Util.exe")
+else
+    os.execute("./Util")
+end
+
+print("\n\nRunning stopped.")
