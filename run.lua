5c07eee02743bcffd5403d2720881b600d8c55bf
Joshua Meng <joshua.meng.xy@gmail.com>
2016-10-20 23:07:30 +0800

    Issue #6: Make run.lua compile & run

--- run.lua
+++ run.lua
@@ -1,9 +1,26 @@
-print("Running started ...\n")
+local application = './Util'
+local cmake_cmd = 'cmake .'
+local make_cmd = 'make'
 
 if strContains(platformInfo(), "win", false) then
-    os.execute("Util.exe")
-else
-    os.execute("./Util")
+    application = 'Util.exe'
+    cmake_cmd = 'cmake -G "MinGW Makefiles'
+    make_cmd = 'mingw32-make'
 end
 
-print("\n\nRunning stopped.")
+print('Make started ...\n')
+
+file.pathRemove(application)
+os.execute(cmake_cmd)
+os.execute(make_cmd)
+
+if not file.isPathFile(application) then
+    print('Make fail.\n')
+    os.exit(-1)
+end
+
+print('Make success.\n')
+
+print("Run app started ...\n")
+os.execute(application)
+print("\n\nRun app stopped.\n")
