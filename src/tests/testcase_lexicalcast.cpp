46319dabc7dbeba51fcdd256bdc684709e48897a
Joshua Meng <joshua.meng.xy@gmail.com>
2016-10-29 00:22:55 +0800

    Add float/double/string test for lexicalCast

--- src/tests/testcase_lexicalcast.cpp
+++ src/tests/testcase_lexicalcast.cpp
@@ -14,8 +14,7 @@ void TestCaseLexicalCast::setUp()
 }
 
 void TestCaseLexicalCast::tearDown()
-{
-    
+{    
 }
 
 void TestCaseLexicalCast::testLexicalCast()
@@ -38,4 +37,17 @@ void TestCaseLexicalCast::testLexicalCast()
     assertEquals<uint64_t>(10, b, ASSERT_POSITION);
     
     assertFalse(lexicalCast<uint64_t>("-10", b), ASSERT_POSITION);
+    
+    float f = 0.0;
+    assertTrue(lexicalCast<float>("-0.2", f), ASSERT_POSITION);
+    assertEquals<float>(-0.2, f, ASSERT_POSITION);
+    
+    double d = 0.0;
+    assertTrue(lexicalCast<double>("2.4E-25", d), ASSERT_POSITION);
+    assertEquals(2.4E-25, d, ASSERT_POSITION);
+    
+    string s;
+    assertTrue(lexicalCast<string>("test a  b\nc", s), ASSERT_POSITION);
+    assertEquals<string>("test a  b\nc", s, ASSERT_POSITION);
 }
+
