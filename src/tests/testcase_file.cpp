503517435d4049b5b3c59947d7413fbaee4709d1
Joshua Meng <joshua.meng.xy@gmail.com>
2016-11-04 23:26:01 +0800

    Issue #5: Add writeTextFile test

--- src/tests/testcase_file.cpp
+++ src/tests/testcase_file.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 using namespace util;
 
+const string kTempFileName = "temp_file";
+
 void TestCaseFile::registerTestFunctions()
 {
     REGISTER_TEST_FUNCTION(TestCaseFile, testReadWrite)
@@ -14,11 +16,19 @@ void TestCaseFile::setUp()
 }
 
 void TestCaseFile::tearDown()
-{    
+{
+    if (isPathFile(kTempFileName))
+        pathRemove(kTempFileName);
 }
 
 void TestCaseFile::testReadWrite()
 {
     assertStringNotWhiteSpace(readTextFile("LICENSE"), ASSERT_POSITION);
+    string content = "This is a test string.";
+    assertTrue(writeTextFile(kTempFileName, content), ASSERT_POSITION);
+    assertEquals(content, readTextFile(kTempFileName), ASSERT_POSITION);
+    assertTrue(writeTextFile(kTempFileName, content), ASSERT_POSITION);
+    assertEquals(content, readTextFile(kTempFileName), ASSERT_POSITION);
+    assertTrue(writeTextFile(kTempFileName, content, true), ASSERT_POSITION);
+    assertEquals(content + content, readTextFile(kTempFileName), ASSERT_POSITION);
 }
-
