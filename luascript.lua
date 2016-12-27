36e94c9390a710f288b007790e4ae588532ec89c
Joshua Meng <joshua.meng.xy@gmail.com>
2016-12-27 15:41:29 +0800

    Rename script tests

---
+++ luascript.lua
@@ -0,0 +1,11 @@
+
+function testFile()
+
+    local re = regex.create("\\d+")
+    if re:search("sbb123gg") then
+        print(re:getMatchedGroupByIndex(0))
+        print(re:getMatchedGroupByIndex(1))
+    end
+end
+
+testFile()
