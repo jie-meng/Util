ca48b6c2a9ac68ec1fbd75e51b37e89d0858090e
Jie Meng <jiemeng@thoughtworks.com>
2017-03-29 14:28:43 +0800

    Add install_luaexeclib.lua

---
+++ install_luaexeclib.lua
@@ -0,0 +1,21 @@
+print('Start install luaexeclib ...')
+
+local idx = string.find(package.path, ';')
+if not idx then
+    print('Install luaexeclib failed.')
+    os.exit(-1)
+end
+
+local first_dir = util.splitPathname(string.sub(package.path, 1, idx-1))
+if util.isPathDir(first_dir) then
+    print('Remove old luaexeclib files from path: ' .. first_dir)
+    os.execute('rm ' .. first_dir .. '/*.*')
+else
+    print('Create path: ' .. first_dir)
+    os.execute('mkdir -p ' .. first_dir)
+end
+
+print('Copy new luaexeclib files to path: ' .. first_dir)
+os.execute('cp -r luaexeclib/. ' .. first_dir)
+
+print('Install luaexeclib finished')
