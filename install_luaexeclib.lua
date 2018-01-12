c992dca6ec827e378a1f1bd9cb5342c5c92412eb
Jie Meng <jiemeng@thoughtworks.com>
2018-01-12 10:24:50 +0800

    Update install_luaexeclib.lua script, make it can run on windows command line

--- install_luaexeclib.lua
+++ install_luaexeclib.lua
@@ -9,15 +9,17 @@ if not idx then
 end
 
 local first_dir = util.splitPathname(string.sub(package.path, 1, idx-1))
-if util.isPathDir(first_dir) then
-    print('Remove old luaexeclib files from path: ' .. first_dir)
-    os.execute('rm ' .. first_dir .. '/*.*')
-else
-    print('Create path: ' .. first_dir)
-    os.execute('mkdir -p ' .. first_dir)
-end
+print('Remove old luaexeclib files from path: ' .. first_dir)
+util.pathRemoveAll(first_dir)
+util.mkDir(first_dir)
 
 print('Copy new luaexeclib files to path: ' .. first_dir)
 os.execute('cp -r luaexeclib/. ' .. first_dir)
+local files = util.findFilesInDirRecursively(util.currentPath() .. '/luaexeclib')
+for _, v in ipairs(files) do
+    local dest = util.strReplace(v, util.currentPath() .. '/luaexeclib', first_dir)
+    print(string.format('Copy "%s"  ---->  "%s"', v, dest))
+    util.fileCopy(v, dest, false)
+end
 
 print('Install luaexeclib finished')
